41de5f91db1a650b7eee41503d45a54e351551ef
Bryan <bryanabc147@gmail.com>
2024-05-27 16:23:00 -0500

    prueba_pr10

--- .github/action/entrypoint.sh
+++ .github/action/entrypoint.sh
@@ -5,8 +5,9 @@ set -euo pipefail
 # Configuration
 INFO_COLOR="\033[34;1m"
 RESET_COLOR="\033[0m"
-ACCEPT_HEADER="Accept: application/vnd.github.v3+json"
-CONTENT_HEADER="Content-Type: application/json"
+
+# Solved detected dubious ownership in repository
+git config --global --add safe.directory /github/workspace
 
 # Helper function to print info messages
 info() {
